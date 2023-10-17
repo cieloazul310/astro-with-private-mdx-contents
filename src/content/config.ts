270aa1c870fb35dc046c3f036bf0c71b0dcd07e7
cieloazul310 <cieloazul310@gmail.com>
2023-10-17 11:18:37 +0900

    initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+
+import { z, defineCollection } from "astro:content";
+
+const postsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      featuredImg: image()
+        .optional()
+        .catch((ctx) => {
+          /* eslint-disable-next-line @typescript-eslint/no-unused-expressions */
+          ctx.error;
+          return undefined;
+        }),
+      featuredImgAlt: z.string().optional(),
+    }),
+});
+
+export const collections = {
+  posts: postsCollection,
+};
---
+++ src/utils/dateString.ts
@@ -0,0 +1,6 @@
+export default function dateString(date: Date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${year}-${month.toString().padStart(2, "0")}-${day}`;
+}
