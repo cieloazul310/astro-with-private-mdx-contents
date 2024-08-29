1ec23600aec69e16513e754b129b20ec137742e6
cieloazul310 <cieloazul310@gmail.com>
2024-08-29 22:07:21 +0900

    fix: fix link

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 import rehypeClassNames from "rehype-class-names";
 import mdxClasses from "./src/components/mdx-classes";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
   markdown: {
     rehypePlugins: [[rehypeClassNames, mdxClasses]],
   },
